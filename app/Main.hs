0c34d4bd6963de178b0a577c2d146117ba82322b
Xavier Shay <contact@xaviershay.com>
2017-10-22 13:10:41 -0700

    It's rendering a hex

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,54 @@
 module Main where
 
 import Lib
+import Graphics.UI.GLUT
+
+hexPoly :: [(Float, Float)]
+hexPoly = [
+    (1.0000, 0.0000),
+    (0.5000, 0.8660),
+    (-0.5000, 0.8660),
+    (-1.0000, 0.0000),
+    (-0.5000, -0.8660),
+    (0.5000, -0.8660),
+    (1.0000, -0.0000)
+  ]
+
+toVertex (x, y) = Vertex3 x y 0.0
+
+display :: DisplayCallback
+display = do
+  -- clear all pixels
+  clear [ ColorBuffer ]
+
+  -- draw white polygon (rectangle) with corners at
+  -- (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
+  color (Color3 1.0 1.0 (1.0 :: GLfloat))
+  -- resolve overloading, not needed in "real" programs
+  let vertex3f = vertex :: Vertex3 GLfloat -> IO ()
+  renderPrimitive Polygon $ mapM_ vertex3f $ map toVertex hexPoly
+
+  -- don't wait!
+  -- start processing buffered OpenGL routines
+  flush
+
+myInit :: IO ()
+myInit = do
+  -- select clearing color
+  clearColor $= Color4 0 0 0 0
+
+  -- initialize viewing values
+  matrixMode $= Projection
+  loadIdentity
+  ortho (-10) 10 (-10) 10 (-1) 1
 
 main :: IO ()
-main = someFunc
+main = do
+  _ <- getArgsAndInitialize
+  initialDisplayMode $= [ SingleBuffered, RGBMode ]
+  initialWindowSize $= Size 500 500
+  initialWindowPosition $= Position 100 100
+  _ <- createWindow "hello"
+  myInit
+  displayCallback $= display
+  mainLoop
--- devnotes.md
+++ devnotes.md
@@ -6,3 +6,7 @@ How to do collisions? _Think_ you need to interpolate and do collision detection
 If you do it instantaneous, two atoms could switch places. But can't just check target destination, otherwise can't have side by side atoms following each other. Maybe could do something drawing lines/arcs and checking for intersection? Based on red square highlighting, suggests OM interpolates.
 
 11:00 Basic grabber rotation sketched out.
+
+(later that day)
+
+1:00 playing around with GLUT for a UI, seems more useful than figuring out ascii hexes
