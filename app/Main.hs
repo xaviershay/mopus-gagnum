3e5feb9ac8ba73f60def5e481d2a1ba51f672174
Xavier Shay <contact@xaviershay.com>
2017-10-29 11:29:40 -0700

    Move hex functions into lib

--- app/Main.hs
+++ app/Main.hs
@@ -38,42 +38,6 @@ gridSize = 5.0
 
 toVertex (x, y) = Vertex3 x y 0.0
 
-hexToPixel :: (Integer, Integer) -> (Float, Float)
-hexToPixel (q, r) =
-  (x, y)
-  where
-    x = sqrt 3 * (fromInteger q + fromInteger r / 2.0)
-    y = 3 / 2 * fromInteger r
-
-pixelToHex :: (Float, Float) -> (Integer, Integer)
-pixelToHex (x, y) =
-  hexRound (q, r)
-  where
-    q = (x * sqrt(3)/3 - y / 3)
-    r = y * 2/3
-
-cubeRound :: (Float, Float, Float) -> (Integer, Integer, Integer)
-cubeRound (x, y, z) =
-    if x_diff > y_diff && x_diff > z_diff then
-      (-ry-rz, ry, rz)
-    else if y_diff > z_diff then
-      (rx, -rx-rz, rz)
-    else
-      (rx, ry, -rx-ry)
-  where
-    rx = (round x) :: Integer
-    ry = (round y) :: Integer
-    rz = (round z) :: Integer
-
-    x_diff = abs (fromIntegral rx - x)
-    y_diff = abs (fromIntegral ry - y)
-    z_diff = abs (fromIntegral rz - z)
-
-axialToCube (q, r) = (q, r, -q-r)
-cubeToAxial (x, y, z) = (x, z)
-
-hexRound :: (Float, Float) -> (Integer, Integer)
-hexRound = cubeToAxial . cubeRound . axialToCube
 
 display :: State -> DisplayCallback
 display state = do
--- src/Lib.hs
+++ src/Lib.hs
@@ -47,6 +47,43 @@ data Board = Board {
 
 placePiece b o p = (b, o, p)
 
+hexToPixel :: (Integer, Integer) -> (Float, Float)
+hexToPixel (q, r) =
+  (x, y)
+  where
+    x = sqrt 3 * (fromInteger q + fromInteger r / 2.0)
+    y = 3 / 2 * fromInteger r
+
+pixelToHex :: (Float, Float) -> (Integer, Integer)
+pixelToHex (x, y) =
+  hexRound (q, r)
+  where
+    q = (x * sqrt(3)/3 - y / 3)
+    r = y * 2/3
+
+cubeRound :: (Float, Float, Float) -> (Integer, Integer, Integer)
+cubeRound (x, y, z) =
+    if x_diff > y_diff && x_diff > z_diff then
+      (-ry-rz, ry, rz)
+    else if y_diff > z_diff then
+      (rx, -rx-rz, rz)
+    else
+      (rx, ry, -rx-ry)
+  where
+    rx = (round x) :: Integer
+    ry = (round y) :: Integer
+    rz = (round z) :: Integer
+
+    x_diff = abs (fromIntegral rx - x)
+    y_diff = abs (fromIntegral ry - y)
+    z_diff = abs (fromIntegral rz - z)
+
+axialToCube (q, r) = (q, r, -q-r)
+cubeToAxial (x, y, z) = (x, z)
+
+hexRound :: (Float, Float) -> (Integer, Integer)
+hexRound = cubeToAxial . cubeRound . axialToCube
+
 buildBoard = Board {
   --tracks = [],
   clock = 0,
@@ -105,8 +142,10 @@ applyAction :: Action -> PlacedPiece -> PlacedPiece
 applyAction (Rotate d) (b, (o, o'), p) = (b, (o', rotateDegrees d o'), p)
 applyAction ClawClose (b, (o, o'), GrabberPiece g@Grabber { closed = False }) =
   (b, (o', o'), GrabberPiece g { closed = True }) 
+
+-- TODO: Place contents on board
 applyAction ClawOpen (b, (o, o'), GrabberPiece g@Grabber { closed = True }) =
-  (b, (o', o'), GrabberPiece g { closed = False }) 
+  (b, (o', o'), GrabberPiece g { closed = False, contents = Nothing }) 
 applyAction _ x = x
   
 
